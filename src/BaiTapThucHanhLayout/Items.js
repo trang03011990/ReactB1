e7849a5f58fac576bd80efad0fa2d5db60452f19
Trang <trang03011990@gmail.com>
2021-10-05 21:38:54 +0700

    init

---
+++ src/BaiTapThucHanhLayout/Items.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+export default class Items extends Component {
+    render() {
+        return (
+            <div className="container my-3">
+                <div className="row text-center">
+                    <div className="col-3 px-0">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Fresh new layout</h5>
+                                <p className="card-text">With Bootstrap 5, we've created a fresh new layout for this template!abc new</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Special title treatment</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Special title treatment</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3 px-0">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Special title treatment</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>          
+                </div>
+            </div>
+
+        )
+    }
+}
